b88da1b0249c35c944eed2cb7d5553471168f308
John Jordan Shelley <jshelley553@gmail.com>
2019-06-04 03:36:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,13 @@
 # Write your #display_rainbow method here
+def pick(color)
+  color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+  puts "The color for R : #{color[0]}"
+    puts "The color for O : #{color[1]}"
+      puts "The color for Y : #{color[2]}"
+        puts "The color for G : #{color[3]}"
+          puts "The color for B : #{color[4]}"
+            puts "The color for I : #{color[5]}"
+            puts "The color for V : #{color[6]}"
+
+end
